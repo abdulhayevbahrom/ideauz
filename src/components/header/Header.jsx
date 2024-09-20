dda4ba9f51d745707d1b38af0c8a2d1169b8266b
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2024-09-20 17:22:36 +0500

    add style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,6 +7,7 @@ import { FaBalanceScale } from "react-icons/fa";
 import { LuUser2 } from "react-icons/lu";
 import "./Header.css";
 import { FiSearch } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -28,28 +29,28 @@ function Header() {
         </div>
 
         <div className="header_links">
-          <div className="header_links_item">
+          <Link to="/heart" className="header_links_item">
             <span>0</span>
             <AiOutlineHeart />
             <p>Tanlanganlar</p>
-          </div>
+          </Link>
 
-          <div className="header_links_item">
+          <Link to="/cart" className="header_links_item">
             <span>0</span>
             <CgShoppingBag />
             <p>Savatcha</p>
-          </div>
+          </Link>
 
-          <div className="header_links_item">
+          <Link to="/compare" className="header_links_item">
             <span>0</span>
             <FaBalanceScale />
             <p>Taqqoslash</p>
-          </div>
+          </Link>
 
-          <div className="header_links_item">
+          <Link to="/login" className="header_links_item">
             <LuUser2 />
             <p>Kirish</p>
-          </div>
+          </Link>
         </div>
       </nav>
     </header>
